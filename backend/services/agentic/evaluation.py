03a15a07d9da11de0144f8bf0d26545717a79db0
Gabriel Yap <gabrielyap37@gmail.com>
2025-09-04 17:13:28 +0800

    Add new model

--- backend/services/agentic/evaluation.py
+++ backend/services/agentic/evaluation.py
@@ -6,10 +6,8 @@ from dataclasses import dataclass
 import logging
 import os
 
-# Set AWS credentials from environment
-os.environ['AWS_ACCESS_KEY_ID'] = os.getenv('AWS_ACCESS_KEY_ID', 'ASIA3M276NSD6ILUPIDR')
-os.environ['AWS_SECRET_ACCESS_KEY'] = os.getenv('AWS_SECRET_ACCESS_KEY', 'i0Oy6R5EDT9M0LjYBsNXMGATh/1HLeaCogY/skCg')
-os.environ['AWS_SESSION_TOKEN'] = os.getenv('AWS_SESSION_TOKEN', 'IQoJb3JpZ2luX2VjEKT//////////wEaDmFwLXNvdXRoZWFzdC0xIkYwRAIgX9w4hYW4P9NGLrzIYkjZbQTYZnTF5vrII31PVCcqz8sCIEIczXlIEEdfMbV3Ryr5XNj3GpzW2dcNAyokbo1tjcg1KowDCP3//////////wEQABoMNzgzNDk1ODgzOTExIgzdHtHQ3PSd2sHLwBAq4AIxfIxGULg8yoBX2N3aTKxokksTJQlML+BglteFj3myBeoNfSBYTNik/YgsTkd2yc0DK4PL52RLBmDOEm2lHk0ib27+FF9DKYwqil3WXjr+p7Azjj//D8b1k1tZP+x8JfDRNPzQV1h4we1isW4amluycAq1hYBap8O/qUwcDv2S5BGHTV55Zq23VkbXBSPZATd/HskWekGFikXsrv5AzNk2zTSF7B/BDN93Q+W6bkmem6Ep4ROP2E1eN+kCmiB1d0DCYd7Yi/q3C/oX9TUwzQlMy7gwaS9puCFs3l8sf3TApDr8R6G+Cx8qaYRGxJNQFzc0KFJpIkXLH8kXBlu9PyKBYvlL9QMJmiR+HW0r1BvqWNf+4p9VmGI/st8d8Dvt5DQsGLikaDc+EVHIud/h3ZsBPd/fdm5TMRzOR8ZD1xSDCzMKCrrsqSgqwn+ah7rdpayNwfZz5lcQy1J5CQIc5tO7MOuu1MUGOqcBLQ6PhpnyRxLVWQh9nlcafBqiXL5XLIHnciNPif1kpeJ5lGo5CfRcbZbYKKlWbOBU0VQJyv0NEwHOWgCBSzgUhSYw7f+Irm1OSB0nnfnxOeCsoU30NqzC2SmcBDxj11SFXvA0TmvXN1fEo187dfMeW/lUnpGEs9I94FcEH2SDNJcnuVlZlsOGbvzLXu3vVO/5IovrCvDfES2rZjdEaQB9nEkbDxvFj/k=')
+# AWS credentials will be loaded from ~/.aws/credentials or environment variables
+# No hardcoded credentials - use aws configure or set AWS_ACCESS_KEY_ID env vars
 
 # Conditional imports for Strands
 try:
